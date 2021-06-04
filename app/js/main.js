994e7a7f4121ab36d5d03f923792025fc1bf5bd7
Nur-sultan79 <nursultanmolzhanuly@gmail.com>
2021-06-04 20:14:00 +0300

    added new sections and failes

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+  
+  $(".top-slider__inner").slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
+  var mixer = mixitup(".products__content");
+
+  var mixer = mixitup(".design-content");
+
+});
