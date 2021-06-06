923ebc84c450785313d70e873ae8c0adf9f18fb9
Nur-sultan79 <nursultanmolzhanuly@gmail.com>
2021-06-06 21:12:13 +0300

    added product.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,26 @@
-$(function(){
-  
+$(function () {
+  $(".filter-price__input").ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $(".filter-price__from").text(data.from);
+      $(".filter-price__to").text(data.to);
+    },
+    onChange: function (data) {
+      $(".filter-price__from").text(data.from);
+      $(".filter-price__to").text(data.to);
+    },
+  });
+
   $(".top-slider__inner").slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
   });
 
   var mixer = mixitup(".products__content");
 
   var mixer = mixitup(".design-content");
-
 });
