3e65b4a32f2ad874155fe3f3c140c6749fabb264
Nur-sultan79 <nursultanmolzhanuly@gmail.com>
2021-06-12 09:04:15 +0300

    added blog page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
 $(function () {
+
+  $(".details-tabs__top-item").on('click', function(e){
+    e.preventDefault();
+    $(".details-tabs__top-item").removeClass("details-tabs__top-item--active");
+    $(this).addClass("details-tabs__top-item--active");
+
+    $(".details-tabs__content-item").removeClass("details-tabs__content-item--active");
+    $($(this).attr('href')).addClass("details-tabs__content-item--active");
+  });
+
   $(".details-slide__thumb").slick({
     asNavFor: ".details-slide__big",
     focusOnSelect: true,
@@ -20,9 +30,9 @@ $(function () {
     slidesToScroll: 1,
     infinite: true,
     prevArrow:
-      '<button class="slick-prev slick-arrow" type="button" style=""><img src="images/icons/line.svg" alt=""></button>',
+      '<button class="slick-prev slick-arrow" type="button" style=""><svg xmlns="http://www.w3.org/2000/svg" width="22" height="9" viewBox="0 0 22 9"><path fill="#436372" d="M1547.3,2629.47h16.94l-0.1,2.86a0.3,0.3,0,0,0-.08.2,0.323,0.323,0,0,0,.08.21l0.18,0.17a0.267,0.267,0,0,0,.4,0l4.2-4.21a0.3,0.3,0,0,0,.08-0.2,0.323,0.323,0,0,0-.08-0.21l-4.2-4.21a0.29,0.29,0,0,0-.4,0l-0.18.17a0.323,0.323,0,0,0-.08.21,0.318,0.318,0,0,0,.08.2l0.12,2.86h-16.97a0.3,0.3,0,0,0-.29.3v1.37A0.291,0.291,0,0,0,1547.3,2629.47Z" transform="translate(-1547 -2624)"/></svg></button>',
     nextArrow:
-      '<button class="slick-next slick-arrow" type="button" style=""><img src="images/icons/line.svg" alt=""></button>',
+      '<button class="slick-next slick-arrow" type="button" style=""><svg xmlns="http://www.w3.org/2000/svg" width="22" height="9" viewBox="0 0 22 9"><path fill="#436372" d="M1547.3,2629.47h16.94l-0.1,2.86a0.3,0.3,0,0,0-.08.2,0.323,0.323,0,0,0,.08.21l0.18,0.17a0.267,0.267,0,0,0,.4,0l4.2-4.21a0.3,0.3,0,0,0,.08-0.2,0.323,0.323,0,0,0-.08-0.21l-4.2-4.21a0.29,0.29,0,0,0-.4,0l-0.18.17a0.323,0.323,0,0,0-.08.21,0.318,0.318,0,0,0,.08.2l0.12,2.86h-16.97a0.3,0.3,0,0,0-.29.3v1.37A0.291,0.291,0,0,0,1547.3,2629.47Z" transform="translate(-1547 -2624)"/></svg></button>',
   });
 
   $(".details__content-input").styler();
